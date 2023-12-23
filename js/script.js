9fb278d85bacf6ade0be23b01880f6d9227d094f
JuanjoMateu <juanjo_mateu@hotmail.es>
2023-12-23 11:40:09 +0100

    js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function backgroundImage() {
     const img = [
-        'url(' + 'assets/img/jase-bloor-oCZHIa1D4EU-unsplash.jpg' + ')',
-        'url(' + 'assets/img/sora-sagano-8sOZJ8JF0S8-unsplash.jpg' + ')',
-        'url(' + 'assets/img/su-san-lee-E_eWwM29wfU-unsplash.jpg' + ')',
-        'url(' + 'assets/img/syuhei-inoue-kaoHI0iHJPM-unsplash.jpg' + ')',
-        'url(' + 'assets/img/tianshu-liu-SBK40fdKbAg-unsplash.jpg' + ')',
+        'url(' + '/assets/img/jase-bloor-oCZHIa1D4EU-unsplash.jpg' + ')',
+        'url(' + '/assets/img/sora-sagano-8sOZJ8JF0S8-unsplash.jpg' + ')',
+        'url(' + '/assets/img/su-san-lee-E_eWwM29wfU-unsplash.jpg' + ')',
+        'url(' + '/assets/img/syuhei-inoue-kaoHI0iHJPM-unsplash.jpg' + ')',
+        'url(' + '/assets/img/tianshu-liu-SBK40fdKbAg-unsplash.jpg' + ')',
     ];
   
     const randomImg = img[Math.floor(Math.random() * img.length)];
